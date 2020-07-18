bf84a4d343df9cf8ee036a79d543d3edc2696671
Luis8792 <67514166+Luis8792@users.noreply.github.com>
2020-07-18 10:38:07 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+function copy() {
+    alert('LOL Avoid Laziness. Try Typing it, rather copying');
+}
+
+function summer() {
+    document.body.style.backgroundImage = "url('https://res.cloudinary.com/pavie/image/upload/v1595005653/summer_jt8yiy.png')"
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "100%";
+    alert("Woo-Hoo! It's Summer");
+}
+
+function winter() {
+    document.body.style.backgroundImage = "url('https://res.cloudinary.com/pavie/image/upload/v1595005659/winter_kxcbyk.png')"
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "100%";
+    alert("Woo-Hoo! It's winter");
+}
+
+function autumn() {
+    document.body.style.backgroundImage = "url('https://res.cloudinary.com/pavie/image/upload/v1595005628/autumn_ywwom5.png')"
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "100%";
+    alert("Woo-Hoo! It's autumn");
+}
+
+function spring() {
+    document.body.style.backgroundImage = "url('https://res.cloudinary.com/pavie/image/upload/v1595005642/spring_qejh9v.png')"
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "100%";
+    alert("Woo-Hoo! It's spring");
+}
